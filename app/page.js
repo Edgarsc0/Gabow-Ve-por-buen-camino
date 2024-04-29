7abe42c559bd50cc7f17cabc5a443ab2e9a75349
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2024-04-29 13:14:44 -0600

    footer agregado con animaciones

--- app/page.js
+++ app/page.js
@@ -1,95 +1,24 @@
-'use client';
-import { Button, Tabs, Box, Text, Heading } from '@radix-ui/themes';
-import { useState } from 'react';
-import {
-  Fade,
-  Slide,
-  Flip,
-  Hinge,
-  JackInTheBox,
-  Roll,
-  Zoom
-} from 'react-awesome-reveal';
+"use client"
+
+import { Zoom } from "react-awesome-reveal"
+import { Heading } from "@radix-ui/themes"
+import { useRouter } from 'next/navigation';
 
 export default function () {
-  const [showGabow, setShowGabow] = useState(false);
+
+  const router = useRouter();
 
   return (
-    <>
-      <Tabs.Root defaultValue='account'>
-        <Slide>
-          <Tabs.List>
-            <Tabs.Trigger value='account'>
-              <Heading
-                weight='regular'
-                className='text-5xl font-bold bg-gradient-to-r from-white to-gray-100 bg-clip-text text-transparent'
-              >
-                Gabow
-              </Heading>
-            </Tabs.Trigger>
-            <Tabs.Trigger value='documents'>
-              <Heading
-                weight='regular'
-                className='text-5xl font-bold bg-gradient-to-r from-white to-gray-100 bg-clip-text text-transparent'
-              >
-                Qu Ex
-              </Heading>
-            </Tabs.Trigger>
-            <Tabs.Trigger value='settings'>
-              <Heading
-                weight='regular'
-                className='text-5xl font-bold bg-gradient-to-r from-white to-gray-100 bg-clip-text text-transparent'
-              >
-                Contact us
-              </Heading>
-            </Tabs.Trigger>
-          </Tabs.List>
-        </Slide>
-
-        <Box pt='3'>
-          <Tabs.Content value='account'>
-            {showGabow ? (
-              <>
-                <Slide>
-                  <Box className='text-center'>
-                    <Heading
-                      size='9'
-                      className='text-5xl font-bold bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent'
-                      onClick={() => setShowGabow(true)}
-                    >
-                      Gabow
-                    </Heading>
-                    <Heading size='5'>Ve por buen camino</Heading>
-                  </Box>
-                </Slide>
-              </>
-            ) : (
-              <Zoom>
-                <div className='flex justify-center items-center h-screen'>
-                  <Heading
-                    size='9'
-                    className='text-5xl font-bold bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent'
-                    onClick={() => setShowGabow(true)}
-                  >
-                    ¡Hola! Comencemos...
-                  </Heading>
-                </div>
-              </Zoom>
-            )}
-          </Tabs.Content>
-          <Tabs.Content value='documents'>
-            <Fade>
-              <Text size='2'>Access and update your documents.</Text>
-            </Fade>
-          </Tabs.Content>
-
-          <Tabs.Content value='settings'>
-            <Text size='2'>
-              Edit your profile or update contact information.
-            </Text>
-          </Tabs.Content>
-        </Box>
-      </Tabs.Root>
-    </>
-  );
+    <Zoom>
+      <div className='flex justify-center items-center h-screen'>
+        <Heading
+          size='9'
+          className='text-5xl font-bold bg-gradient-to-r from-red-500 to-purple-600 bg-clip-text text-transparent'
+          onClick={() => router.push("/GetStarted")}
+        >
+          ¡Hola! Comencemos...
+        </Heading>
+      </div>
+    </Zoom>
+  )
 }
