e36ba9818cf7c110812051a6e96383e46882e560
Edgarsc0 <esanchezc2004@alumno.ipn.mx>
2024-05-27 15:23:09 -0600

    modified db condifg

--- app/db/db.js
+++ app/db/db.js
@@ -1,10 +1,10 @@
 import pg from "pg"
 
 const config = {
-    user: process.env.POSTGRES_USER || process.env.DB_USER,
-    host: process.env.PPOSTGRES_HOST || process.env.DB_HOST,
-    password: process.env.POSTGRES_PASSWORD || process.env.DB_PASSWORD,
-    database: process.env.POSTGRES_DATABASE || process.env.DB_DATABASE,
+    user: process.env.POSTGRES_USER,
+    host: process.env.PPOSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     port: 5432,
     ssl: {
         rejectUnauthorized: false
